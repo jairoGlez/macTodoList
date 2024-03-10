be8cee0643ee2f55bec6b5dc0f13e8bc5a7a968a
Jairo Gonzalez <jairo.glez@hotmail.com>
2024-03-10 15:50:22 -0600

    Added xcode files

---
+++ macTodoList/macTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  macTodoListApp.swift
+//  macTodoList
+//
+//  Created by Jairo Gonzalez on 10/03/24.
+//
+
+import SwiftUI
+
+@main
+struct macTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
